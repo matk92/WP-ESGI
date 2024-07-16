e09a5dbba5809073bd946defb3e8960c6111b294
Mathieu Keca <mathieu.keca@gmail.com>
2024-07-16 16:08:44 +0200

    update

--- ESGI-theme/footer.php
+++ ESGI-theme/footer.php
@@ -1,30 +1,20 @@
-<footer id="site-footer" class="col-md-6 offset-3">
-    <div class="container">
-        ©<?= date('Y') ?> Edouard Sombié
-        <?php if (has_nav_menu('footer_menu')) : ?>
-            <nav id="footer-navigation" class="footer-menu" role="navigation">
-                <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer_menu',
-                        'menu_id'        => 'footer-menu',
-                    ));
-                ?>
-            </nav>
-        <?php endif; ?>
-
-        <?php if (is_active_sidebar('footer-widget-area')) : ?>
-            <div class="footer-widgets">
-                <?php dynamic_sidebar('footer-widget-area'); ?>
+<footer>
+        <div class="footer-container">
+            <div class="footer-content">
+                <div class="contact-info">
+                    <p>Manager: <a href="tel:+33153312523">+33 1 53 31 25 23</a> | <a href="mailto:info@esgi.com">info@esgi.com</a></p>
+                    <p>CEO: <a href="tel:+33153312525">+33 1 53 31 25 25</a> | <a href="mailto:ceo@company.com">ceo@company.com</a></p>
+                </div>
+                <div class="social-links">
+                    <a href="https://www.linkedin.com"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                </div>
+                <div class="copyright">
+                    <p>© 2022 Figma Template by ESGI</p>
+                </div>
             </div>
-        <?php endif; ?>
-
-        <?php if (get_theme_mod('has_footer_search', false)) : ?>
-            <div class="footer-search">
-                <?php get_search_form(); ?>
-            </div>
-        <?php endif; ?>
-    </div>
-</footer>
-<?php wp_footer(); ?>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
