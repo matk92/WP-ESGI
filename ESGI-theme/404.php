39f1edea090ead07f493629e221b2aa53abe4a25
Mathieu Keca <mathieu.keca@gmail.com>
2024-07-16 17:25:41 +0200

    update

--- ESGI-theme/404.php
+++ ESGI-theme/404.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 <main id="site-main-wp" class="site-main-wp">
-    <div class="container">
         <div class="row">
             <h1>404 Error</h1>
                 <p>The page you were looking for couldn't be found.<br>
@@ -8,6 +7,5 @@
                 </p>
             <?php get_search_form(); ?>
         </div>
-    </div>
 </main>
 <?php get_footer(); ?>
