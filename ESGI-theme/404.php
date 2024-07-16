f48515cf4369814bf99f5a11b3e7b0e78c5a74a6
Remy Saguez <resaguez.2800@gmail.com>
2024-07-16 16:38:57 +0200

    404

--- ESGI-theme/404.php
+++ ESGI-theme/404.php
@@ -2,8 +2,10 @@
 <main id="site-main-wp" class="site-main-wp">
     <div class="container">
         <div class="row">
-            <h1>Page Introuvable</h1>
-                <p>Essayez de faire une recherche</p>
+            <h1>404 Error</h1>
+                <p>The page you were looking for couldn't be found.<br>
+                    Maybe try a search?
+                </p>
             <?php get_search_form(); ?>
         </div>
     </div>
